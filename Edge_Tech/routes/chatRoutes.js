e2b37d81dc95a9e73ad4a1dbd5bc26622b106297
Pawan8538 <pawanpatidar8538@gmail.com>
2025-02-23 10:52:30 +0530

    Update chatRoutes.js

--- Edge_Tech/routes/chatRoutes.js
+++ Edge_Tech/routes/chatRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const { handleChat } = require('../controllers/chatController');
-const Chat = require('../models/chat');
 
 router.post('/chat', handleChat);
 
